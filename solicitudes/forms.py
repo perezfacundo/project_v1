f0250d0304a8d8b1375e0cc7455807d4debc753f
Facundo <perezfaq98@gmail.com>
2023-07-05 22:12:28 -0300

    sync 05/07

--- solicitudes/forms.py
+++ solicitudes/forms.py
@@ -1,7 +1,12 @@
 from django.forms import ModelForm
 from .models import Solicitud
+from django import forms
+
 
 class SolicitudForm(ModelForm):
     class Meta:
         model = Solicitud
         fields = ['desde', 'hasta', 'fechaTrabajo', 'detalles']
+        widgets = {
+            'title': forms.
+        }
