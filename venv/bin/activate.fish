f80c48a06c86475e27d67d3da33d049398765041
Facundo Perez <63246299+perezfacundo@users.noreply.github.com>
2023-11-01 11:26:18 -0300

    sync no venv

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/facundoperez/Documentos/project_v1/venv"
+set -gx VIRTUAL_ENV "/home/facundoperez/Documentos/Proyectos/project_v1/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
