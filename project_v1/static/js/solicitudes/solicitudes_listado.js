65e1e341df43bd06452e0b3f56b03476c2b0e28e
Facundo <perezfaq98@gmail.com>
2023-11-24 11:01:48 -0300

    sync

--- project_v1/static/js/solicitudes/solicitudes_listado.js
+++ project_v1/static/js/solicitudes/solicitudes_listado.js
@@ -3,55 +3,53 @@ let dataTableIsInitialized = false;
 
 // Configuracion de la datatable
 const dataTableOptions = {
-    columnDefs: [
-        { className: 'centered', targets: [0, 1, 2, 3, 4, 5, 6, 7] },
-        { orderable: false, targets: [5, 6, 7] },
-        { searchable: false, targets: [5, 6, 7] }
-    ],
-    destroy: true,
-    dom: 'Bfrtip',
-    buttons: [
-        'copy', 'csv', 'excel', 'pdf', 'print'
-    ]
-}
+  columnDefs: [
+    { className: "centered", targets: [0, 1, 2, 3, 4, 5, 6, 7] },
+    { orderable: false, targets: [5, 6, 7] },
+    { searchable: false, targets: [5, 6, 7] },
+  ],
+  destroy: true,
+  dom: "Bfrtip",
+  buttons: ["copy", "csv", "excel", "pdf", "print"],
+};
 
 // Inicializacion de la datatable
 const initDataTable = async () => {
-    if (dataTableIsInitialized) {
-        if (dataTable) {
-            dataTable.destroy();
-        }
+  if (dataTableIsInitialized) {
+    if (dataTable) {
+      dataTable.destroy();
     }
-    await listSolicitudes();
+  }
+  await listSolicitudes();
 
-    try {
-        dataTable = $('#tableSolicitudes').DataTable(dataTableOptions);
-    } catch (ex) {
-        alert(ex)
-    }
+  try {
+    dataTable = $("#tableSolicitudes").DataTable(dataTableOptions);
+  } catch (ex) {
+    alert(ex);
+  }
 
-    dataTableIsInitialized = true;
-}
+  dataTableIsInitialized = true;
+};
 
-// Proceso de informacion 
+// Proceso de informacion
 const listSolicitudes = async () => {
-    try {
-        const response = await fetch('http://127.0.0.1:8000/solicitudes_listado/');
-        const data = await response.json();
-        const estados = data.estados
-        const usuario = data.usuario
+  try {
+    const response = await fetch("http://127.0.0.1:8000/solicitudes_listado/");
+    const data = await response.json();
+    const estados = data.estados;
+    const usuario = data.usuario;
 
-        let bodyContent = '';
-        let btnCalificar = '';
-        let btnDetalles = '';
-        let btnEliminar = '';
+    let bodyContent = "";
+    let btnCalificar = "";
+    let btnDetalles = "";
+    let btnEliminar = "";
 
-        data.solicitudes.forEach((solicitud) => {
-            btnCalificar = `<a class="btn btn-sm " style="background-color:#3B4C7D;" href="http://127.0.0.1:8000/solicitudes/calificar/${solicitud.id}/"><i class="bi bi-star-fill" style="color:#FFFFFF"></i></a>`;
-            btnDetalles = `<a class="btn btn-sm " style="background-color:#357266;" href="http://127.0.0.1:8000/solicitudes/${solicitud.id}/"><i class="bi bi-info-circle-fill" style="color:#FFFFFF"></i></a>`;
-            btnEliminar = `<a class="btn btn-sm " style="background-color:#C44558;" href="http://127.0.0.1:8000/solicitudes/eliminar/${solicitud.id}"/><i class="bi bi-trash-fill" style="color:#FFFFFF"></i></a>`;
-            
-            bodyContent += `
+    data.solicitudes.forEach((solicitud) => {
+      btnCalificar = `<a class="btn btn-sm " style="background-color:#3B4C7D;" href="http://127.0.0.1:8000/solicitudes/calificar/${solicitud.id}/"><i class="bi bi-star-fill" style="color:#FFFFFF"></i></a>`;
+      btnDetalles = `<a class="btn btn-sm " style="background-color:#357266;" href="http://127.0.0.1:8000/solicitudes/${solicitud.id}/"><i class="bi bi-info-circle-fill" style="color:#FFFFFF"></i></a>`;
+      btnEliminar = `<a class="btn btn-sm " style="background-color:#C44558;" href="http://127.0.0.1:8000/solicitudes/eliminar/${solicitud.id}"/><i class="bi bi-trash-fill" style="color:#FFFFFF"></i></a>`;
+
+      bodyContent += `
                 <tr>
                     <td>${solicitud.id}</td>
                     <td>${solicitud.cliente}</td>
@@ -59,32 +57,35 @@ const listSolicitudes = async () => {
                     <td>${solicitud.direccion_hasta}</td>
                     <td>${solicitud.fecha_trabajo}</td>
                     <td>${solicitud.estado}</td>
-                    <td>${btnDetalles}`
+                    <td>${btnDetalles}
+            `;
 
-            if (solicitud.estado == "Entregado") {
-                if (usuario.tipo_usuario == "Cliente") {
-                    bodyContent += `${btnCalificar}`
-                }
-            }
+      if (solicitud.estado == "Entregado") {
+        if (usuario.tipo_usuario == "Cliente") {
+          bodyContent += `${btnCalificar}`;
+        }
+      }
 
-            bodyContent += `</td><td>`;
-            if (solicitud.calificacion != null) {
-                for (var i = 1; i <= solicitud.calificacion; i++) {
-                    bodyContent += `
+      bodyContent += `</td><td>`;
+      if (solicitud.calificacion != null) {
+        for (var i = 1; i <= solicitud.calificacion; i++) {
+          bodyContent += `
                         <i class="bi bi-star-fill" style="color:#F8DA62;"></i>
-                    `
-                }
-            } else { bodyContent += `sin calificar` } bodyContent += `</td></tr>`
-        });
+                    `;
+        }
+      } else {
+        bodyContent += `sin calificar`;
+      }
+      bodyContent += `</td></tr>`;
+    });
 
-        tableBody.innerHTML = bodyContent;
-    } catch (ex) {
-        alert(ex);
-    }
+    tableBody.innerHTML = bodyContent;
+  } catch (ex) {
+    alert(ex);
+  }
 };
 
-
 // Escucha del evento load
-window.addEventListener('load', async () => {
-    await initDataTable();
-})
+window.addEventListener("load", async () => {
+  await initDataTable();
+});
--- project_v1/static/js/solicitudes/solicitudes_reportes.js
+++ project_v1/static/js/solicitudes/solicitudes_reportes.js
@@ -1,146 +1,167 @@
 let dataTable;
 let dataTableIsInitialized = false;
 
-let arrayEjeX = []
-let arrayEjeY = []
+let arrayEjeX = [];
+let arrayEjeY = [];
+
+let fechaInicio;
+let fechaFin;
 
 let option = {
-    'tooltip': {
-        'show': true,
-        'trigger': "axis",
-        'triggerOn': "mousemove|click"
+  tooltip: {
+    show: true,
+    trigger: "axis",
+    triggerOn: "mousemove|click",
+  },
+  title: {
+    text: "Solicitudes",
+    // subtext: `Desde el ${desde} hasta el ${hasta}`
+  },
+  toolbox: {
+    feature: {
+      saveAsImage: { show: true },
     },
-    'toolbox': {
-        'feature': {
-          'saveAsImage': { show: true }
-        }
+  },
+  xAxis: [
+    {
+      type: "category",
+      data: arrayEjeX,
+      axisLabel: { rotate: 30 },
     },
-    'xAxis': [
-        {
-            'type': "category",
-            'data': arrayEjeX,
-            'axisLabel': { rotate: 30}
-        }
-    ],
-    'yAxis': [
-        {
-            'type': "value"
-        }
-    ],
-    'series': [
-        {
-            'data': arrayEjeY,
-            'type': "bar"
-        }
-    ]
-}
+  ],
+  yAxis: [
+    {
+      type: "value",
+    },
+  ],
+  series: [
+    {
+      data: arrayEjeY,
+      type: "bar",
+    },
+  ],
+};
 
 const dataTableOptions = {
-    columnDefs: [
-        { orderable: false, targets: [1] },
-    ],
-    "searching": false,
-    dom: 'Bfrtip',
-    buttons: [
-        'copy', 'csv', 'excel', 'pdf', 'print'
-    ]
+  columnDefs: [{ orderable: false, targets: [1] }],
+  searching: false,
+  dom: "Bfrtip",
+  buttons: ["copy", "csv", "excel", "pdf", "print"],
 };
 
 // Función para inicializar la DataTable
 const initDataTable = async () => {
-    if (dataTableIsInitialized) {
-        if (dataTable) {
-            dataTable.destroy();
-        }
-    };
-
-    await listReportes();
-
-    try {
-        dataTable = $('#tableSolicitudes').DataTable(dataTableOptions)
-    } catch (error) {
-        alert(ex)
+  if (dataTableIsInitialized) {
+    if (dataTable) {
+      dataTable.destroy();
     }
+  }
 
-    dataTableIsInitialized = true;
+  await listReportes();
+
+  try {
+    dataTable = $("#tableSolicitudes").DataTable(dataTableOptions);
+  } catch (error) {
+    alert(ex);
+  }
+
+  dataTableIsInitialized = true;
 };
 
 const listReportes = async () => {
-    try {
-        $('#enviarButton').click(function () {
-            const fechaInicio = $('#fechaInicio').val();
-            const fechaFin = $('#fechaFin').val();
-            const csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+  try {
+    $("#enviarButton").click(function () {
+      fechaInicio = $("#fechaInicio").val();
+      fechaFin = $("#fechaFin").val();
 
-            const url = 'http://127.0.0.1:8000/solicitudes/reportes/'
+      const csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
 
-            const datos = {
-                fechaInicio: fechaInicio,
-                fechaFin: fechaFin,
-                // csrfmiddlewaretoken: csrfToken
-            };
+      const url = "http://127.0.0.1:8000/solicitudes/reportes/";
 
-            const config = {
-                method: 'POST',
-                headers: {
-                    'X-CSRFToken': csrfToken,
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(datos)
-            }
+      const datos = {
+        fechaInicio: fechaInicio,
+        fechaFin: fechaFin,
+      };
 
-            fetch(url, config)
-                .then(response => response.json())
-                .then(data => {
-                    let bodyContent = "";
-                    let total = 0;
+      const config = {
+        method: "POST",
+        headers: {
+          "X-CSRFToken": csrfToken,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(datos),
+      };
 
-                    data.estados.forEach((estado) => {
-                        total += estado.cantidad
+      fetch(url, config)
+        .then((response) => response.json())
+        .then((data) => {
+          let bodyContent = "";
+          let total = 0;
 
-                        bodyContent += `
+          data.estados.forEach((estado) => {
+            total += estado.cantidad;
+
+            bodyContent += `
                             <tr>
                                 <td>${estado.descripcion}</td>
-                                <td>${estado.cantidad}</td>
+                                <td class="centered">${estado.cantidad}</td>
                             </tr>
                         `;
 
-                        arrayEjeX.push(estado.descripcion)
-                        arrayEjeY.push(estado.cantidad)
-                    })
+            arrayEjeX.push(estado.descripcion);
+            arrayEjeY.push(estado.cantidad);
+          });
 
-                    bodyContent += `
+
+          bodyContent += `
                         <tr>
                             <td><strong>Total</strong></td>
-                            <td>${total}</td>
+                            <td class="centered">${total}</td>
                         </tr>
-                    `
+                    `;
 
-                    tableBody.innerHTML = bodyContent;
+          tableBody.innerHTML = bodyContent;
 
-                    initChart();
 
-                    reiniciarOption();
-                })
-                .catch(error => console.error('Error:', error))
-        });
-    } catch (ex) {
-        alert(ex);
-        console.log("Error: ", ex)
-    };
+          initChart();
+
+          reiniciarOption();
+        })
+        .catch((error) => console.error("Error:", error));
+    });
+  } catch (ex) {
+    alert(ex);
+    console.log("Error: ", ex);
+  }
 };
 
+document.addEventListener("DOMContentLoaded", function () {
+    //Obtener la fecha de hoy
+    var fechaHoy = new Date();
+
+    // Establecer el valor del campo de la fecha de hoy
+    var fechaHoyInput = document.getElementById("fechaFin");
+    fechaHoyInput.valueAsDate = fechaHoy;
+
+    //Obtener el primer dia del mes
+    var primerDiaMes = new Date(fechaHoy.getFullYear(), fechaHoy.getMonth(), 1);
+
+    //Establecer el valor del campo del primer dia del mes
+    var primerDiaMesInput = document.getElementById("fechaInicio");
+    primerDiaMesInput.valueAsDate = primerDiaMes;
+  });
+
 window.addEventListener("load", async () => {
-    await initDataTable();
+  await initDataTable();
 });
 
 const initChart = () => {
-    let myChart = echarts.init(document.getElementById("chart"));
-    myChart.clear();
-    myChart.setOption(option);
-}
+  let myChart = echarts.init(document.getElementById("chart"));
+  myChart.clear();
+  myChart.setOption(option);
+};
 
 function reiniciarOption() {
-    arrayEjeX.splice(0, arrayEjeX.length);
-    arrayEjeY.splice(0, arrayEjeY.length);
+  arrayEjeX.splice(0, arrayEjeX.length);
+  arrayEjeY.splice(0, arrayEjeY.length);
 }
