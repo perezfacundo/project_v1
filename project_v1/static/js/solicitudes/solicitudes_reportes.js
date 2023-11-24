408c1e0e8631aac70440e46b95b4a1b45f34fe83
Facundo <perezfaq98@gmail.com>
2023-11-24 12:03:55 -0300

    sync

--- project_v1/static/js/solicitudes/solicitudes_reportes.js
+++ project_v1/static/js/solicitudes/solicitudes_reportes.js
@@ -135,22 +135,6 @@ const listReportes = async () => {
   }
 };
 
-document.addEventListener("DOMContentLoaded", function () {
-    //Obtener la fecha de hoy
-    var fechaHoy = new Date();
-
-    // Establecer el valor del campo de la fecha de hoy
-    var fechaHoyInput = document.getElementById("fechaFin");
-    fechaHoyInput.valueAsDate = fechaHoy;
-
-    //Obtener el primer dia del mes
-    var primerDiaMes = new Date(fechaHoy.getFullYear(), fechaHoy.getMonth(), 1);
-
-    //Establecer el valor del campo del primer dia del mes
-    var primerDiaMesInput = document.getElementById("fechaInicio");
-    primerDiaMesInput.valueAsDate = primerDiaMes;
-  });
-
 window.addEventListener("load", async () => {
   await initDataTable();
 });
